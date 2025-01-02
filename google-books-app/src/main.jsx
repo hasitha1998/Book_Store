d79843ec2d65cc1c9564412aac24a84cba28d881
Hasitha Sanjaya <79785680+hasitha1998@users.noreply.github.com>
2025-01-02 10:00:08 +0530

    feat: integrate Header and BookCard components into the main App and chore: wrap the app with BooksContext provider

--- google-books-app/src/main.jsx
+++ google-books-app/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import BooksProvider from './context/BooksContext';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BooksProvider>
     <App />
-  </StrictMode>,
-)
+  </BooksProvider>
+);
